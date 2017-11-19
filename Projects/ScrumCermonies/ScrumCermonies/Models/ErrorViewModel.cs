f8ed89f0573e7c8e55d5b5f3cff49b6251abeebe
shadeegun <shadeegun@hotmail.co.uk>
2017-11-19 21:34:50 +0000

    Adding ScrumCeremonies base project to repository

---
+++ Projects/ScrumCermonies/ScrumCermonies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ScrumCermonies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
